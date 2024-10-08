e3be00d18fa435137625f2b07c2dffb483852855
chaeyopa <bcy4601@gmail.com>
2024-10-08 15:27:03 +0900

    1008text

---
+++ newlibft/ft_strncmp.cpp
@@ -0,0 +1,9 @@
+#include "libft.hpp"
+
+using namespace std;
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	
+	return 0;
+}
---
+++ 수업필기_과제/2410/20241008_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// 1. 2중 for문과 배열을 이용하여 다음처럼 숫자를 출력하라
+/*
+1	2	3	4	5
+6	7	8	9	10
+11	12	13	14	15
+16	17	18	19	20
+21	22	23	24	24
+*/
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
