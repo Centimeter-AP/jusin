66c3c6b7f69e34d5ba793b291f73d03f485d73b5
chaeyopa <bcy4601@gmail.com>
2024-10-18 09:15:37 +0900

    17.5

---
+++ 수업필기_과제/2410/20241017_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	FILE	*fOrigin = nullptr;
+	FILE	*fCopy = nullptr;
+	int		*sCopy = new int[128];
+
+	fOrigin = fopen("./Image.jpg", "rb");
+	fCopy = fopen("./Copied.jpg", "wb");
+
+	if (fOrigin && fCopy)
+	{
+		while (!feof(fOrigin))
+		{
+			fread(sCopy, sizeof(sCopy), 100, fOrigin);
+			fwrite(sCopy, sizeof(sCopy), 100, fCopy);
+		}
+	}
+	else
+	{
+		cout << "something went wrong" << endl;
+	}
+	delete[] sCopy;
+	fclose(fOrigin);
+	fclose(fCopy);
+	return 0;
+}
